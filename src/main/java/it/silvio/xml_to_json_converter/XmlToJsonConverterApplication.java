dd915da82442f5416349503463eb6bdfb2e5b03d
SLP <silviolepera@gmail.com>
2024-09-04 02:09:10 +0200

    first commit

---
+++ src/main/java/it/silvio/xml_to_json_converter/XmlToJsonConverterApplication.java
@@ -0,0 +1,13 @@
+package it.silvio.xml_to_json_converter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XmlToJsonConverterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XmlToJsonConverterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/silvio/xml_to_json_converter/XmlToJsonConverterApplicationTests.java
@@ -0,0 +1,13 @@
+package it.silvio.xml_to_json_converter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XmlToJsonConverterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
